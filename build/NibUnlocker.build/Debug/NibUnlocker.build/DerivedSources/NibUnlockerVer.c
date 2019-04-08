1d274d01cd557772a0a437f1dfe4370ca36b0e11
Andy Vandijck <andyvand@gmail.com>
2019-04-08 18:37:46 +0200

    Added partial sources

---
+++ build/NibUnlocker.build/Debug/NibUnlocker.build/DerivedSources/NibUnlockerVer.c
@@ -0,0 +1,5 @@
+ extern const unsigned char NibUnlockerVersionString[];
+ extern const double NibUnlockerVersionNumber;
+
+ const unsigned char NibUnlockerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:NibUnlocker  PROJECT:NibUnlocker-0.2.1" "\n";
+ const double NibUnlockerVersionNumber __attribute__ ((used)) = (double)0.2;
